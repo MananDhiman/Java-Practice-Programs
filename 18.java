2fd3048e16b7a4a6ec8d045d40f68e041f0f76d2
Manan Dhiman <manandheman@gmail.com>
2023-06-05 17:51:06 +0530

    two thirds done

--- 18.java
+++ 18.java
@@ -4,14 +4,23 @@ class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("");
+        System.out.printf("Enter Number of Elements in array: ");
+        int n = sc.nextInt();
+        
+        int[] nums = new int[n];
+        
+        System.out.printf("Enter "+n+" Elements to add in first array: ");
+        for(int i=0;i<n;i++) nums[i] = sc.nextInt();
 
-        double num = sc.nextDouble();
-        
+        int[] newArr = new int[n];
+
+        for(int i=0; i<n; i++) newArr[i] = nums[i];
+
+        System.out.println("Iterating new array");
+        for(int i: newArr) System.out.printf("%d ",i);
+
         sc.close();
-        
     }
 }
-
 
 // Design a program to copy an array by iterating the array.
